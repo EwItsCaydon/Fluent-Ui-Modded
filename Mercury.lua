7838b69e9681b4c1cd401d3890767ef3e028e22f
EwItsCaydon <caydonnahm@gmail.com>
2024-12-21 20:13:23 -0500

    Update Mercury.lua

--- Mercury.lua
+++ Mercury.lua
@@ -799,7 +799,7 @@ function Library:create(options)
 		Theme = {TextColor3 = {"WeakText", -20}},
 		TextScaled = true,
 		TextXAlignment = Enum.TextXAlignment.Left,
-		Text = tostring(os.date("%X")):sub(1, os.date("%X"):len()-3)
+		Text = tostring(Date)
 	})
 
 	do
