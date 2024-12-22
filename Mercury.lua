bd18fb6e46d871b08f8fbb823462a67d7dcd70e6
EwItsCaydon <caydonnahm@gmail.com>
2024-12-22 00:52:13 -0500

    Update Mercury.lua

--- Mercury.lua
+++ Mercury.lua
@@ -1099,7 +1099,7 @@ end
 function Library:tab(options)
 	options = self:set_defaults({
 		Name = "New Tab",
-		Icon = "rbxassetid://110743212392098"
+		Icon = "rbxassetid://8569322835"
 	}, options)
 
 	local tab = self.container:object("ScrollingFrame", {
